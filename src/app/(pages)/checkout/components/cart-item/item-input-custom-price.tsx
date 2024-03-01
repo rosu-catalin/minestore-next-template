d6f36a45c8d7f745b313aa79a4e308e2dc02570b
Catalin Rosu <zonetriplu@gmail.com>
2024-03-01 20:12:15 +0200

    Added debouncing for custom price input

--- src/app/(pages)/checkout/components/cart-item/item-input-custom-price.tsx
+++ src/app/(pages)/checkout/components/cart-item/item-input-custom-price.tsx
@@ -3,9 +3,20 @@ import { Input } from '@/components/base/input/input';
 import { Label } from '@layout/label/label';
 import { useCartItemPreferences } from '@/app/(pages)/categories/utils/use-cart-item-preferences';
 import { DescriptionTooltip } from './item-description-tooltip';
+import { useDebounceCallback } from 'usehooks-ts';
+import { notify } from '@/core/notifications';
 
 export function InputItemCustomPrice({ item }: { item: TCart['items'][number] }) {
     const { handleSetCustomPrice } = useCartItemPreferences();
+
+    const debouncedHandleSetProductVariable = useDebounceCallback((id, price, min_price) => {
+        if (price < min_price) {
+            notify('The price must be higher than the minimum price!', 'red');
+            return;
+        }
+
+        handleSetCustomPrice({ id, price });
+    }, 500);
 
     if (!item.is_any_price) return null;
 
@@ -14,9 +25,7 @@ export function InputItemCustomPrice({ item }: { item: TCart['items'][number] })
             <div className="flex items-center justify-between">
                 <Label htmlFor="custom-price">Custom Price</Label>
                 <DescriptionTooltip
-                    description={
-                        'Set a custom price for the item. The price must be higher than the minimum price.'
-                    }
+                    description={`${item.name} has a minimum price of ${item.min_price} USD `}
                 />
             </div>
             <Input
@@ -25,10 +34,7 @@ export function InputItemCustomPrice({ item }: { item: TCart['items'][number] })
                 placeholder={item.price.toString()}
                 min={item.min_price}
                 onChange={(e) =>
-                    handleSetCustomPrice({
-                        id: item.id,
-                        price: Number(e.target.value)
-                    })
+                    debouncedHandleSetProductVariable(item.id, e.target.value, item.min_price)
                 }
             />
         </div>
